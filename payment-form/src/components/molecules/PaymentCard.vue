a60f696e7560a2a28edcd87b4354e9d44941454d
NormanRDD <43154261+NormanRDD@users.noreply.github.com>
2023-12-21 23:30:31 +0600

    commit

---
+++ payment-form/src/components/molecules/PaymentCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <component :is="dynamicComponent"></component>
+</template>
+
+<script>
+import { ref, defineComponent } from "vue";
+// import form from "@/json/form.json";
+export default defineComponent({
+  props: {
+    componentDefinition: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup(props) {
+    const dynamicComponent = ref(props.componentDefinition);
+
+    return {
+      dynamicComponent,
+    };
+  },
+});
+</script>
